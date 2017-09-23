3aae85e70082fda535952be9aaab278a5c38c4e8
kryx07 <kryx07@gmail.com>
2017-09-23 19:45:00 +0200

    Comments added.

--- src/main/java/com/kryx07/cache/item/CacheItem.java
+++ src/main/java/com/kryx07/cache/item/CacheItem.java
@@ -1,8 +1,13 @@
 package com.kryx07.cache.item;
 
 public interface CacheItem {
-
+    /**
+     * @return the key of the cache item - a unique identifier of the item within the underlying cache's collection.
+     */
     String getKey();
 
+    /**
+     * @return the value of the cached item - the underlying Object
+     */
     Object getValue();
 }
